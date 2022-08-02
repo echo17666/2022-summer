28d59871b69bf6324b6cc5eece7a675235637b94
echo17666 <72566693+echo17666@users.noreply.github.com>
2022-08-02 17:17:38 +0800

    重命名团队

--- frontend/src/components/Teams/AllTeams.vue
+++ frontend/src/components/Teams/AllTeams.vue
@@ -1,13 +1,22 @@
 <template>
   <div class="allteams">
     <h1>所有团队列表</h1>
-    <v-btn @click="$router.push({name:'TeamProject'})"> 某个团队 </v-btn>
+    <v-row>
+    <v-col cols="12" md="4" v-for="(team,index) in teams" :key="index">
+        <team :team="team"></team>
+      </v-col>
+      </v-row>
+       <v-btn @click="$router.push({name:'TeamProject'})"> 某个团队 </v-btn>
   </div>
 </template>
 <script>
 import { Team } from "@/api/team.js";
+import TeamItem from "../Starting/TeamItem.vue"
 export default {
   name: 'AllTeams',
+   components: {
+    team:TeamItem
+  },
   data(){
     return{
       teams:[]
