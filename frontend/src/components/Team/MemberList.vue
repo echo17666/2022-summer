1929f86bb1bf17a46ccce0f3fb1c985ea6a637d9
your-user-name <your-email-address-on-gitee>
2022-08-02 10:52:27 +0800

    1

--- frontend/src/components/Team/MemberList.vue
+++ frontend/src/components/Team/MemberList.vue
@@ -8,12 +8,69 @@
     </v-row>
 
     <v-dialog v-model="dialog" persistent max-width="600px">
+      <v-card
+          color="black"
+          dark
+      >
+        <v-card-title class="headline red lighten-3">
+          Search for Public APIs
+        </v-card-title>
+        <v-card-text>
+          Explore hundreds of free API's ready for consumption! For more information visit
+          <a
+              class="grey--text text--lighten-3"
+              href="https://github.com/toddmotto/public-apis"
+              target="_blank"
+          >the Github repository</a>.
+        </v-card-text>
+        <v-card-text>
+          <v-autocomplete
+              v-model="model"
+              :items="items"
+              :loading="isLoading"
+              :search-input.sync="search"
+              color="white"
+              hide-no-data
+              hide-selected
+              item-text="Description"
+              item-value="API"
+              label="Public APIs"
+              placeholder="Start typing to Search"
+              prepend-icon="mdi-database-search"
+              return-object
+          ></v-autocomplete>
+        </v-card-text>
+        <v-divider></v-divider>
+        <v-expand-transition>
+          <v-list v-if="model" class="red lighten-3">
+            <v-list-item
+                v-for="(field, i) in fields"
+                :key="i"
+            >
+              <v-list-item-content>
+                <v-list-item-title v-text="field.value"></v-list-item-title>
+                <v-list-item-subtitle v-text="field.key"></v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+        </v-expand-transition>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+              :disabled="!model"
+              color="grey darken-3"
+              @click="model = null"
+          >
+            Clear
+            <v-icon right>mdi-close-circle</v-icon>
+          </v-btn>
+        </v-card-actions>
+      </v-card>
     </v-dialog>
 
     <v-btn  @click="dialog=!dialog" class="mx-2" fab dark color="indigo">
       <span class="material-icons-outlined">add</span>
     </v-btn>
-
 
   </div>
 </template>
