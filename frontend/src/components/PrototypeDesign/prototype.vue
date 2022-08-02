110e30d43afd5751e0287dba2877bb1bee450c73
orz010 <3320642802@qq.com>
2022-08-02 11:29:02 +0800

    123

---
+++ frontend/src/components/PrototypeDesign/prototype.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="parent">
+        <drag-it-dude>
+            <div class="inner">
+                <h1>here!</h1> 
+            </div>
+        </drag-it-dude>
+    </div>
+</template>
+<script>
+    import DragItDude from "vue-drag-it-dude";
+    export default{
+        name: "prototype",
+        components:{
+            DragItDude
+        },
+
+    }
+</script>
+<style>
+#parent{
+    position: relative;
+}
+</style>
