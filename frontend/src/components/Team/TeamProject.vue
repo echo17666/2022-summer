459b07c3ecb5deabd5c37c7f1c24615c818cf06b
your-user-name <your-email-address-on-gitee>
2022-08-02 10:45:28 +0800

    style

--- frontend/src/components/Team/TeamProject.vue
+++ frontend/src/components/Team/TeamProject.vue
@@ -1,8 +1,6 @@
 <template>
   <div class="teamproject" :style="{'margin-left':'10px','margin-right':'10px'}">
-    <h1>团队项目列表</h1>  <v-btn  @click="dialog=!dialog" class="mx-2" fab dark color="indigo">
-    <span class="material-icons-outlined">add</span>
-  </v-btn>
+    <h1>团队项目列表</h1>
     <v-row>
       <v-col cols="12" md="4" v-for="(project,index) in project" :key="index">
         <proitem :project="project"></proitem>
@@ -45,6 +43,9 @@
       </v-card>
     </v-dialog>
 
+    <v-btn  @click="dialog=!dialog" class="mx-2" fab dark color="indigo">
+      <span class="material-icons-outlined">add</span>
+    </v-btn>
 
   </div>
 
