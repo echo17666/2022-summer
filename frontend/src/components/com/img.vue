41c94661e28a0fe345a568af1f07b2f486229f1a
echo17666 <72566693+echo17666@users.noreply.github.com>
2022-08-03 21:04:53 +0800

    改了些图形

--- frontend/src/components/com/img.vue
+++ frontend/src/components/com/img.vue
@@ -2,8 +2,8 @@
   <div class="design">
         <VueDragResize  v-show="info.isshown" :isActive="true" :w="info.width" :h="info.height" :x="info.left" :y="info.top" v-on:resizing="resize" v-on:dragging="resize">
             <div :style="{'position':'relative'}" style="height:100%">
-                <v-img src="https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fi.qqkou.com%2Fi%2F2a2548060212x3232476469b26.jpg&refer=http%3A%2F%2Fi.qqkou.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1662105148&t=dbc7bd1acb83f48165e768c718b8de0e"  
-                :width="info.width" :height="info.height" ></v-img>
+                <img src="../../assets/img.png"
+                width="100%" height="100%" />
                 <v-btn @click="deleted(info)" :style="{'position':'absolute','top':'0px','right':'0px'}" icon> <span class="material-icons-outlined">
                         close</span></v-btn>
             </div>
