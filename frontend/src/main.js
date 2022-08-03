37dc4ca7b70d431401b191c795871ebdfa643535
echo17666 <72566693+echo17666@users.noreply.github.com>
2022-08-03 11:28:46 +0800

    拖拽组件尝试

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -7,6 +7,8 @@ import vuetify from './plugins/vuetify'
 import material from 'material-icons/iconfont/material-icons.css';
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import VueDragResize from 'vue-drag-resize'
+
 
 Vue.config.productionTip = false
 import { ElementTiptapPlugin } from "element-tiptap";
@@ -14,7 +16,7 @@ import 'element-ui/lib/theme-chalk/index.css';
 // 引入 import element-tiptap 样式
 import 'element-tiptap/lib/index.css';
 Vue.use(ElementUI)
-
+Vue.component('vue-drag-resize', VueDragResize)
 Vue.prototype.$axios = axios;
 Vue.use( ElementTiptapPlugin, {
   lang: 'zh', // 设置语言为中文
