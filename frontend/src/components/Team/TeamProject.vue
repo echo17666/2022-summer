e7edde9eb1141205b28db6b26b082f97cc370454
your-user-name <your-email-address-on-gitee>
2022-08-03 15:36:43 +0800

    style

--- frontend/src/components/Team/TeamProject.vue
+++ frontend/src/components/Team/TeamProject.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="teamproject" :style="{'margin-left':'10px','margin-right':'10px'}">
     <h1>团队项目列表</h1>
+    <div style="height: 10px"></div>
     <v-row>
       <v-col cols="12" md="4" v-for="(project,index) in project"
              :key="index"
              v-if="project.deleted===false"
+             style="background-color: lightblue;"
       >
         <proitem :project="project"></proitem>
       </v-col>
