e3268dcb1210e43ae8bafe651bb2b7c9c82ae6f2
your-user-name <your-email-address-on-gitee>
2022-08-10 09:35:03 +0800

    Update Manage.vue

--- frontend/src/components/Docs/Manage.vue
+++ frontend/src/components/Docs/Manage.vue
@@ -118,7 +118,7 @@
             <v-card-actions>
               <v-spacer></v-spacer>
               <v-btn color="blue darken-1" text @click="cancel()">取消</v-btn>
-              <v-btn color="blue darken-1" text @click="addFolder();getDocument()">添加</v-btn>
+              <v-btn color="blue darken-1" text @click="addFolder()">添加</v-btn>
             </v-card-actions>
           </v-card>
         </v-dialog>
@@ -210,6 +210,7 @@ export default {
         document.createfolder(formdata)
             .then((response) => {
               console.log(response.data)
+              this.getDocument();
               this.$notify({
                 title: '添加文件夹成功',
                 type: 'success'
@@ -219,6 +220,7 @@ export default {
               this.dialog1=false;
               this.dialog=false;
               this.folder_id=0;
+              this.getDocument();
             })
             .catch((error) => {
               console.log(error)
@@ -249,6 +251,7 @@ export default {
         document.createdocument(formdata)
             .then((response) => {
               console.log(response.data)
+              this.getDocument();
               this.$notify({
                 title: '添加文档成功',
                 type: 'success'
@@ -257,7 +260,7 @@ export default {
               this.dialog=false;
               this.dialog1=false;
               this.DocumentName=""
-              this.getDocument()
+              this.getDocument();
             })
             .catch((error) => {
               console.log(error)
